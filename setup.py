73e606441eed3a616971bf6f4edea09d4638f5f9
Jannis Leidel <jannis@leidel.info>
2010-06-16 13:02:45 +0200

    Added default packaging files.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.md')
+
+setup(
+    name = "django-phased",
+    version = "0.1a1",
+    url = 'http://github.com/codysoyland/django-phased',
+    license = 'BSD',
+    description = "Simple two-phase template rendering application useful for caching of authenticated requests.",
+    long_description = README,
+    author = 'Cody Soyland',
+    author_email = 'codysoyland@gmail.com',
+    packages = [
+        'phased',
+        'phased.templatetags',
+    ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
