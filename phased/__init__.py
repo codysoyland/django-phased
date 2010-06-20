029a159fe3f920d59e0168af72177b343daa4256
Jannis Leidel <jannis@leidel.info>
2010-06-20 13:41:20 +0200

    Make use of Django's hashcompat module.

--- phased/__init__.py
+++ phased/__init__.py
@@ -1,10 +1,7 @@
 from django.conf import settings
+from django.utils.hashcompat import sha_constructor
 
 def generate_secret_delimiter():
-    try:
-        from hashlib import sha1
-    except ImportError:
-        from sha import sha as sha1
-    return sha1(getattr(settings, 'SECRET_KEY', '')).hexdigest()
+    return sha_constructor(getattr(settings, 'SECRET_KEY', '')).hexdigest()
 
 LITERAL_DELIMITER = getattr(settings, 'LITERAL_DELIMITER', generate_secret_delimiter())
