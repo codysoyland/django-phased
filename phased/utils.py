7dd2abcdf741a91148327f0d696fa4bdfd227e13
Jannis Leidel <jannis@leidel.info>
2012-06-29 11:11:01 +0200

    Revert "Fixed issue #1 -- Added ability to automatically load the template tag libraries that are used during the first pass of rendering."
    This reverts commit e0552688432cbf59fa293ed64df4f668db1b7e62.

--- phased/utils.py
+++ phased/utils.py
@@ -1,14 +1,13 @@
-import re, base64, itertools
+import re, base64
 from django.conf import settings as django_settings
-from django.http import HttpRequest
-from django.template import (Parser, Lexer, Token, import_library,
-    TOKEN_TEXT, COMMENT_TAG_START, COMMENT_TAG_END, TemplateSyntaxError)
 from django.template.context import BaseContext, RequestContext, Context
+from django.template import (Parser, Lexer, Token,
+    TOKEN_TEXT, COMMENT_TAG_START, COMMENT_TAG_END, TemplateSyntaxError)
 from django.utils.cache import cc_delim_re
 from django.utils.functional import Promise, LazyObject
-from django.utils.encoding import smart_str
-
+from django.http import HttpRequest
 from django.contrib.messages.storage.base import BaseStorage
+from django.utils.encoding import smart_str
 
 try:
     import cPickle as pickle
@@ -17,15 +16,8 @@ except ImportError:
 
 from phased import settings
 
-pickled_context_re = re.compile(r'.*%s context "(.*)" endcontext %s.*' % (COMMENT_TAG_START, COMMENT_TAG_END))
-pickled_components_re = re.compile(r'.*%s components "(.*)" endcomponents %s.*' % (COMMENT_TAG_START, COMMENT_TAG_END))
-
+pickled_context_re = re.compile(r'.*%s stashed context: "(.*)" %s.*' % (COMMENT_TAG_START, COMMENT_TAG_END))
 forbidden_classes = (Promise, LazyObject, HttpRequest, BaseStorage)
-forbidden_components = (
-    'django.template.defaultfilters',
-    'django.template.defaulttags',
-    'django.template.loader_tags'
-)
 
 def second_pass_render(request, content):
     """
@@ -41,17 +33,11 @@ def second_pass_render(request, content):
             tokens = Lexer(bit, None).tokenize()
         else:
             tokens.append(Token(TOKEN_TEXT, bit))
-        # restore the previos context including the CSRF token
+
         context = RequestContext(request,
             restore_csrf_token(request, unpickle_context(bit)))
-        # restore the loaded components (tags and filters)
-        parser = Parser(tokens)
-        unpickled_components = unpickle_components(bit) or []
-        for component in unpickled_components:
-            lib = import_library(component)
-            parser.add_library(lib)
-        # render the piece with the restored context
-        rendered = parser.parse().render(context)
+        rendered = Parser(tokens).parse().render(context)
+
         if settings.SECRET_DELIMITER in rendered:
             rendered = second_pass_render(request, rendered)
         result.append(rendered)
@@ -83,17 +69,6 @@ def backup_csrf_token(context, storage=None):
         storage = Context()
     storage['csrf_token'] = smart_str(context.get('csrf_token', 'NOTPROVIDED'))
     return storage
-
-def find_components(parser):
-    """
-    Return a list of template tags library dotted paths to stash it away for
-    later automagic loading during the second rendering.
-    """
-    modules = set()
-    for func in itertools.chain(parser.tags.itervalues(), parser.filters.itervalues()):
-        if func.__module__ not in forbidden_components:
-            modules.add(func.__module__)
-    return list(modules)
 
 def drop_vary_headers(response, headers_to_drop):
     """
@@ -162,27 +137,7 @@ def pickle_context(context, template=None):
     """
     if not isinstance(context, BaseContext):
         raise TemplateSyntaxError('Phased context is not a Context instance')
-    if template is None:
-        template = '{# context "%s" endcontext #}'
     pickled_context = pickle.dumps(flatten_context(context), protocol=pickle.HIGHEST_PROTOCOL)
-    return template % base64.standard_b64encode(pickled_context)
-
-def pickle_components(components, template=None):
-    """
-    Pickle the list of components and base64 them.
-    """
     if template is None:
-        template = '{# components "%s" endcomponents #}'
-    pickled_components = pickle.dumps(components, protocol=pickle.HIGHEST_PROTOCOL)
-    return template % base64.standard_b64encode(pickled_components)
-
-def unpickle_components(content, pattern=None):
-    """
-    Unpickle the components from the given content string or return None.
-    """
-    if pattern is None:
-        pattern = pickled_components_re
-    match = pattern.search(content)
-    if match:
-        return pickle.loads(base64.standard_b64decode(match.group(1)))
-    return None
+        template = '{# stashed context: "%s" #}'
+    return template % base64.standard_b64encode(pickled_context)
