696306dc10222111eaa5bd58fdad064e5ecdfd2c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-07-10 16:47:54 +0200

    Add phasedcache template tag
    phasedcache acts like Django's cache template tag, but does a second
    pass render over the cached content

--- phased/test_settings.py
+++ phased/test_settings.py
@@ -25,3 +25,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+)
